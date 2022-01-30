5188c40972f574c991f2b7edbc3130a6327a3847
Takagi Hiroto <hiroju.work@gmail.com>
2022-01-30 17:07:57 +0900

    クエリ名修正

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,12 +1,17 @@
 import { EntityPost } from 'entities/post'
+import { GraphQLClient } from 'graphql-request'
 import { wpGraphqlClient } from 'libs/graphql'
-import { getPosts } from 'types/graphql/post'
+import { PostBy, Posts } from 'types/graphql/post'
 import { IServicePost } from 'usecases/post'
 
 export class ServicePost implements IServicePost {
+  private driver: GraphQLClient
+  constructor() {
+    this.driver = wpGraphqlClient
+  }
   async getPosts(page: number, perPage: number) {
     const offset = Math.max((page - 1) * perPage, 0)
-    const res = await wpGraphqlClient.request(getPosts, {
+    const res = await this.driver.request(Posts, {
       offset,
       size: perPage,
     })
